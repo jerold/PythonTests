2f84695b7991dd4b6968bfb95a94b343061d420d
Jerold <jerold.albertson@gmail.com>
2015-01-03 10:53:05 -0700

    non-functioning work

--- planetShadows.py
+++ planetShadows.py
@@ -1,3 +1,5 @@
+import math
+
 
 # Point has a simple (x,y)
 class Point:
@@ -14,11 +16,14 @@ class Body:
 # Function checks whether or not p2 (Point)
 # is hidden from p1 (Point) behind b (Body)
 def isHidden(p1, p2, b):
+    q = math.asin(b.radius/(b.center.x-p1.x)) # angle from p1 to external tangent of body
+    print q
+    k = math.atan((p2.y-p1.y)/(p2.x-p1.x)) # angle from p1 to p2
+    print k
     return True
 
-
 # Tests
-print True == isHidden(Point(0, 0), Point(20, 0), Body(5, Point(10, 0)))
-print False == isHidden(Point(0, 0), Point(10, 0), Body(5, Point(20, 0)))
-print False == isHidden(Point(0, 0), Point(20, 20), Body(5, Point(10, 0)))
-print False == isHidden(Point(0, 0), Point(20, -20), Body(5, Point(10, 0)))
+print True == isHidden(Point(0.0, 0.0), Point(20.0, 0.0), Body(5.0, Point(10.0, 0.0)))
+print False == isHidden(Point(0.0, 0.0), Point(10.0, 0.0), Body(5.0, Point(20.0, 0.0)))
+print False == isHidden(Point(0.0, 0.0), Point(20.0, 20.0), Body(5.0, Point(10.0, 0.0)))
+print False == isHidden(Point(0.0, 0.0), Point(20.0, -20.0), Body(5.0, Point(10.0, 0.0)))
